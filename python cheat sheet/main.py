6df638e484e8b26ec48ad5bda1824705cfa9b363
Brian Connolly <brian.connolly71@gmail.com>
2024-07-05 12:17:02 +0100

    initial commit

---
+++ python cheat sheet/main.py
@@ -0,0 +1,21 @@
+# variables are dynamically typed
+n - 0
+print('n=', n)
+
+# types are determined at run tme
+n = "abc"
+print('n =', n)
+
+# Multiple assignments
+n, m = 0, "abc"
+n, m, z = 0.125, "abc", False
+
+# Increment
+n = n + 1 # good
+n += 1    # good
+n++       # bad
+
+# None is null (absence of value)
+n = 4
+n = None
+print("n =", n)
